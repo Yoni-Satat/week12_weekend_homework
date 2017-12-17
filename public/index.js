1c3db8b3d8597d31f191f79982d74f75b4215bf0
Yoni Satat <yoni.satat@gmail.com>
2017-12-17 16:47:58 +0000

    amended create door all 3 functions

--- public/index.js
+++ public/index.js
@@ -4,23 +4,23 @@ const app = function() {
 
   const createDoors = document.querySelector('#create-doors');
   createDoors.addEventListener('click', function() {
-    doorNumberOne();
-    doorNumberTwo();
-    doorNumberThree();
+    doorNumberOne('blue');
+    doorNumberTwo('blue');
+    doorNumberThree('blue');
   });
 
-  const doorNumberOne = function() {
-    context.fillStyle = 'blue';
+  const doorNumberOne = function(fill) {
+    context.fillStyle = fill;
     context.fillRect(75,125,200,350);
   }
 
-  const doorNumberTwo = function() {
-    context.fillStyle = 'blue';
+  const doorNumberTwo = function(fill) {
+    context.fillStyle = fill;
     context.fillRect(325,125,200,350);
   }
 
-  const doorNumberThree = function() {
-    context.fillStyle = 'blue';
+  const doorNumberThree = function(fill) {
+    context.fillStyle = fill;
     context.fillRect(575,125,200,350);
   }
 
